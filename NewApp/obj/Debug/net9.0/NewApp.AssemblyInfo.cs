d49741161d19bffe2b5f0a82c3021385e8bca6ae
“phamvanlinh133” <phamvanlinh133@gmail.com>
2025-01-17 19:26:43 +0700

    add Attributes.html

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e42a8ff6769fcf7fbafb7319410f9b064c612d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fe278c6929ae63452465c78398eb0e96c448ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
