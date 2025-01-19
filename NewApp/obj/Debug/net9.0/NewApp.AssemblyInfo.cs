fc70e08f5103315c3206df264e022d5155ce6bcc
“phamvanlinh133” <phamvanlinh133@gmail.com>
2025-01-19 22:17:03 +0700

    add css

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2557f406229f5cda546c0404703be23195546eef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0b53a6bae693d7ba3f7ad50ae2edfce4b475d04")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
