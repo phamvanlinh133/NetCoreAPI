576bf213d28630223d3e59c80bb097bb8083e199
“phamvanlinh133” <phamvanlinh133@gmail.com>
2025-03-10 14:54:02 +0700

    CreateProject

--- PTPMQL/DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ PTPMQL/DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e786db0aa8719cb929d70341bbe7e66ef5022ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d8206125b281ccce60f2f24754207350511c55")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
